096881691e5564617eda8dc4fc4984b9e1950491
Artur <artur.oliveira9876@gmail.com>
2026-07-23 10:52:16 -0300

    fix(ui): fixed flip card animation

--- ui/src/components/table/Board.tsx
+++ ui/src/components/table/Board.tsx
@@ -5,8 +5,8 @@ export function Board({cards, pot, rake}: { cards: string[]; pot: number; rake?:
     className="pot-value">{pot.toLocaleString('pt-BR')}</b>{rake ?
     <small title="Comissão da casa cobrada sobre o pote (rake)"
       aria-label={`Comissão da casa: ${rake.toLocaleString('pt-BR')} fichas`}>rake {rake.toLocaleString('pt-BR')}</small> : null}</span>
-  <div>{cards.map((card, index) => <PlayingCard key={`${index}-${card}`} card={card} index={index}
-    size="board"/>)}{Array.from({length: 5 - cards.length}, (_, i) =>
+  <div>{cards.map((card, index) => <PlayingCard key={`${index}-${card}`} card={card}
+    index={index < 3 ? index : 0} size="board"/>)}{Array.from({length: 5 - cards.length}, (_, i) =>
     <span key={i}/>)}</div>
   </div>;
 }
